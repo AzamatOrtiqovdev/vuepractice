b16b8f398d11a93cde440301f6f815e613017873
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2023-05-21 23:39:34 +0500

    add components

---
+++ vue-project/src/components/movie-add-form/MovieAddForm.vue
@@ -0,0 +1,12 @@
+<template>
+      <h1>Movie Add Form</h1>
+</template>
+
+<script> 
+      export default {
+
+      }
+</script>
+
+<style> 
+</style>
---
+++ vue-project/src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,11 @@
+<template>
+      <h1>Search Panel</h1>
+</template>
+
+<script>
+      export default {
+
+      }
+</script>
+
+<style scoped></style>
