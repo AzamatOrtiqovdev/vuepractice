ef34dacf9e080d65fbd08b2ccf1e12e3386140db
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2023-05-22 12:09:56 +0500

    add style to movielistitem

--- vue-project/src/components/movie-list-item/MovieListItem.vue
+++ vue-project/src/components/movie-list-item/MovieListItem.vue
@@ -1,5 +1,19 @@
 <template>
-      <li>John Wick 1</li>
+      <li class="list-group-item d-flex justify-content-between">
+            <span class="list-group-item-label">John Wick 1</span>
+            <input type="number" class="list-group-item-input" defaultValue="811">
+
+            <div class="d-flex justify-content-center align-items-center">
+                  <button type="button" class="btn-cookie btn-sm">
+                        <i class="fas fa-cookie"></i>
+                  </button>
+                  <button type="button" class="btn-trash btn-sm">
+                        <i class="fas fa-trash"></i>
+                  </button>
+                  <i class="fas fa-star"></i>
+            </div>
+
+      </li>
 </template>
 
 <script>
@@ -9,5 +23,69 @@
 </script>
 
 <style scoped>
+      .list-group-item {
+            padding: 15px 20px;
+            border: none;
+            background-color: transparent;
+            border-bottom: 1px solid #3d5a80;
+      }
+
+      .list-group-item:last-child {
+            border-bottom: none;
+      }
+
+      .list-group-item span {
+            line-height: 35px;
+            font-size: 20px;
+            cursor: pointer;
+            width: 550px;
+      }
+
+      .list-group-item input {
+            line-height: 35px;
+            font-size: 20px;
+            text-align: center;
+            border: 0;
+            outline: none;
+      }
+
+      .list-group-item button {
+            width: 35px;
+            height: 35px;
+            margin: 3px;
+            font-size: 17px;
+            border: none;
+            cursor: pointer;
+      }
+
+      .list-group-item .btn-cookie {
+            color: #e09f3f;
+      }
+
+      .list-group-item .btn-trash {
+            color: #e5383b;
+      }
+
+      .list-group-item .fa-star {
+            width: 35px;
+            height: 35px;
+            text-align: center;
+            line-height: 35px;
+            font-size: 16px;
+            color: #fffd80;
+            transition: .3s all;
+            transform: translateX(35px);
+            opacity: 0;
+      }
+
+      .list-group-item.like .fa-star {
+            transform: translateX(0);
+            opacity: 1;
+      }
+
+      .list-group-item.favourite .list-group-item-label,
+      .list-group-item.favourite .list-group-item-input {
+            color: #e09f3f;
+      }
 
 </style>
